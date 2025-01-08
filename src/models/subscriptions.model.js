8318f2316434de5de98d28f4c30aaca752c4228f
WhySamir <csit23081025_samir@achsnepal.edu.np>
2025-01-08 15:30:26 +0545

    add subscriptions model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
+
+export const subscriptions = mongoose.model(
+  "subscriptions",
+  subscriptionSchema
+);
