cbe3f50af3a11b2b3b5951d0b6f9b9c6bb1f003b
WhySamir <csit23081025_samir@achsnepal.edu.np>
2025-01-16 15:17:30 +0545

    update app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ import videoRouter from "./routes/video.routes.js";
 
 //routes declaration .usefor middleware and controller access not provided segmently by app.get as routeris accessed customly
 app.use("/api/v1/users", userRouter); //router,controller
-app.use("/api/v1/users", userRouter); //router,controller
+app.use("/api/v1/video", videoRouter); //router,controller
 
 export { app };
